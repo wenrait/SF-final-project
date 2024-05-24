c3860b5c53baac4b8f3e7be5358133f1d165317a
Dmitry <wenrait@gmail.com>
2024-05-25 02:23:34 +0300

    Added Auth container visible to unauthorised

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
-import { colors } from '../styles/globalStyles.ts';
+import { colors, fonts } from '../styles/globalStyles.ts';
 import { LogoComponent } from '../components/Logo.tsx';
+import authDivider from '../assets/svg/auth__divider.svg';
 
 const HeaderWrapper = styled.header`
   width: 100%;
@@ -19,6 +20,7 @@ const HeaderContent = styled.div`
   justify-content: center;
   align-items: center;
   position: relative;
+  font-size: 14px;
 `;
 
 const HeaderNav = styled.nav`
@@ -38,6 +40,43 @@ const LogoWrapper = styled.div`
   left: 60px;
 `;
 
+const AuthContainer = styled.div`
+  position: absolute;
+  right: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const AuthButtons = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+`;
+
+const AuthLink = styled.a`
+  text-decoration: none;
+  color: ${colors.primary.black};
+  opacity: 0.4;
+`;
+
+const AuthButton = styled.button`
+  border: 0;
+  color: ${colors.primary.black};
+  font-family: ${fonts.Inter};
+  background-color: ${colors.secondary.lightTeal};
+  cursor: pointer;
+  border-radius: 5px;
+  padding: 5px 10px;
+  font-weight: 500;
+  font-size: 14px;
+`;
+
+const AuthDivider = styled.img`
+  display: block;
+`;
+
 export const HeaderComponent = () => {
   return (
     <HeaderWrapper>
@@ -50,6 +89,13 @@ export const HeaderComponent = () => {
           <StyledNavLink to={'/'}>Тарифы</StyledNavLink>
           <StyledNavLink to={'/'}>FAQ</StyledNavLink>
         </HeaderNav>
+        <AuthContainer>
+          <AuthButtons>
+            <AuthLink href={'/'}>Зарегистрироваться</AuthLink>
+            <AuthDivider src={authDivider} alt={''} />
+            <AuthButton>Войти</AuthButton>
+          </AuthButtons>
+        </AuthContainer>
       </HeaderContent>
     </HeaderWrapper>
   );
