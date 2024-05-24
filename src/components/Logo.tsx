bb356811755a0fb0e48cc7b7b652b46a2c33ff30
Dmitry <wenrait@gmail.com>
2024-05-25 01:14:17 +0300

    Added LogoComponent in Logo.tsx

---
+++ src/components/Logo.tsx
@@ -0,0 +1,21 @@
+import HeaderLogo from '../assets/svg/logo__header.svg';
+import FooterLogo from '../assets/svg/logo__footer.svg';
+import styled from 'styled-components';
+
+const Logo = styled.img`
+  display: block;
+`;
+
+export interface LogoComponentsProps {
+  place: 'header' | 'footer';
+}
+
+export const LogoComponent = ({ place }: LogoComponentsProps) => {
+  const logo = place === 'header' ? HeaderLogo : FooterLogo;
+
+  return (
+    <div>
+      <img src={logo} alt={'СКАН'} />
+    </div>
+  );
+};
