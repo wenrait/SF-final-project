7290b769b22447147d7f91d7ed6e53996af424d7
Dmitry <wenrait@gmail.com>
2024-05-28 13:36:48 +0300

    Imported Home and Footer components into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import { HeaderComponent } from './layout/Header.tsx';
+import { HomeComponent } from './pages/Home.tsx';
+import { Routes, Route } from 'react-router-dom';
+import { FooterComponent } from './layout/Footer.tsx';
 
 function App() {
+  const ErrorMock = () => {
+    return <div>Error</div>;
+  };
+
   return (
     <>
       <HeaderComponent />
+      <Routes>
+        <Route path={'/'} element={<HomeComponent />} />
+        <Route path={'/*'} element={<ErrorMock />} />
+      </Routes>
+      <FooterComponent />
     </>
   );
 }
