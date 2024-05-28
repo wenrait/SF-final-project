ef0a05311910cbef20773c5d633c77117d48ad2e
Dmitry <wenrait@gmail.com>
2024-05-28 13:34:57 +0300

    Added Footer component

---
+++ src/layout/Footer.tsx
@@ -0,0 +1,68 @@
+import styled from 'styled-components';
+import { colors } from '../styles/globalStyles.ts';
+import { LogoComponent } from '../components/Logo.tsx';
+
+const FooterWrapper = styled.header`
+  width: 100%;
+  height: 137px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: ${colors.primary.teal};
+  color: ${colors.primary.white};
+`;
+
+const FooterContent = styled.div`
+  display: flex;
+  width: 100%;
+  max-width: 1320px;
+  padding: 0 60px;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  font-size: 14px;
+`;
+
+const LogoWrapper = styled.div`
+  position: absolute;
+  left: 60px;
+`;
+
+const FooterRight = styled.div`
+  position: absolute;
+  right: 60px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  text-align: end;
+`;
+
+const Contacts = styled.div``;
+
+const Copyright = styled.div`
+  font-size: 12px;
+`;
+
+export const FooterComponent = () => {
+  return (
+    <FooterWrapper>
+      <FooterContent>
+        <LogoWrapper>
+          <LogoComponent place={'footer'} />
+        </LogoWrapper>
+        <FooterRight>
+          <Contacts>
+            <span>
+              г. Москва, Цветной б-р, 40
+              <br />
+              +7 495 771 21 11
+              <br />
+              info@skan.ru
+            </span>
+          </Contacts>
+          <Copyright>Copyright, 2022</Copyright>
+        </FooterRight>
+      </FooterContent>
+    </FooterWrapper>
+  );
+};
