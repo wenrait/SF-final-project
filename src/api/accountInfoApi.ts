d93a62457b4760f2f954bf31420002dee74831a4
Dmitry <wenrait@gmail.com>
2024-06-09 19:50:03 +0300

    Added Account Info API

---
+++ src/api/accountInfoApi.ts
@@ -0,0 +1,28 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { AccInfoResponse } from '../types.ts';
+import { RootState } from '../redux/store.ts';
+
+export const accountInfoApi = createApi({
+  reducerPath: 'accountInfoApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_API_URL,
+    prepareHeaders: (headers, { getState }) => {
+      const state = getState() as RootState;
+      const accessToken = state.authReducer.accessToken;
+
+      if (accessToken) {
+        headers.set('Authorization', `Bearer ${accessToken}`);
+      }
+    },
+  }),
+  endpoints: (builder) => ({
+    getAccountInfo: builder.query<AccInfoResponse, unknown>({
+      query: () => ({
+        url: import.meta.env.VITE_ACCOUNT_INFO,
+        method: 'GET',
+      }),
+    }),
+  }),
+});
+
+export const { useGetAccountInfoQuery } = accountInfoApi;
