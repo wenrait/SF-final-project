380c80d54e32dd5cdfcbd1b960ab8f9ea1d51990
Dmitry <wenrait@gmail.com>
2024-06-10 23:42:52 +0300

    1) Imported separated Carousel component; 2) Added conditional rendering to Button; 3) Implemented adaptive layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,10 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import homeSvg1 from './../assets/svg/home__1.svg';
 import homeSvg2 from './../assets/svg/home__2.svg';
-import carouselSvg1 from './../assets/svg/Home__Carousel--1.svg';
-import carouselSvg2 from './../assets/svg/Home__Carousel--2.svg';
-import carouselSvg3 from './../assets/svg/Home__Carousel--3.svg';
-import arrowLeft from './../assets/svg/Home__Carousel__arrow--left.svg';
-import arrowRight from './../assets/svg/Home__Carousel__arrow--right.svg';
 import { TariffComponent } from '../components/Tariff.tsx';
 import { ButtonComponent } from '../components/Button.tsx';
-// import { usePostAuthDataMutation } from '../api/login-api.ts';
+import { CarouselComponent } from '../components/Carousel.tsx';
+import { useAppSelector } from '../hooks.ts';
 
 const Home = styled.div`
   width: 100%;
@@ -21,12 +17,19 @@ const Content = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-  max-width: 1320px;
+  max-width: 1440px;
   padding: 0 60px;
   justify-content: center;
   align-items: center;
   position: relative;
   gap: 100px;
+  box-sizing: border-box;
+  @media (max-width: 960px) {
+    padding: 0 30px;
+  }
+  @media (max-width: 600px) {
+    padding: 0 14px;
+  }
 `;
 
 const Header = styled.section`
@@ -34,7 +37,9 @@ const Header = styled.section`
   width: 100%;
   justify-content: space-between;
   align-items: center;
-  position: relative;
+  @media (max-width: 960px) {
+    flex-wrap: wrap;
+  }
 `;
 
 const HeaderLeft = styled.div`
@@ -50,11 +55,26 @@ const HeaderTitle = styled.h1`
   line-height: 72px;
   margin: 0;
   text-transform: uppercase;
+  @media (max-width: 960px) {
+    font-size: 45px;
+    line-height: 54px;
+  }
+  @media (max-width: 600px) {
+    font-size: 28px;
+    line-height: 34px;
+  }
 `;
 
 const HeaderDescription = styled.p`
   font-size: 20px;
   margin: 0 0 50px;
+`;
+
+const HeaderSvg = styled.img`
+  width: 45%;
+  @media (max-width: 960px) {
+    width: 100%;
+  }
 `;
 
 const Section = styled.section`
@@ -70,58 +90,24 @@ const Subtitle = styled.h2`
   font-size: 45px;
   text-transform: uppercase;
   font-family: Ferry, Arial, sans-serif;
-`;
-
-const Carousel = styled.div`
-  width: 100%;
-  display: flex;
-  padding: 0 40px;
-  gap: 30px;
-  position: relative;
-  box-sizing: border-box;
-`;
-
-const CarouselItem = styled.div`
-  box-shadow: 0 0 20px 0 #00000033;
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-  padding: 30px 20px;
-  font-size: 18px;
-  border-radius: 10px;
-  flex: 1;
-`;
-
-const StyledImage = styled.img`
-  width: fit-content;
-`;
-
-const CarouselButtonCSS = css`
-  padding: 0;
-  background: transparent;
-  border: 0;
-  cursor: pointer;
-  position: absolute;
-  top: calc(50% - 20px);
-`;
-
-const LeftButton = styled.button`
-  ${CarouselButtonCSS};
-  left: 0;
-`;
-
-const RightButton = styled.button`
-  ${CarouselButtonCSS};
-  right: 0;
+  @media (max-width: 960px) {
+    font-size: 28px;
+    line-height: 34px;
+  }
 `;
 
 const Tariffs = styled.div`
   display: flex;
   gap: 30px;
   margin-bottom: 100px;
+  flex-wrap: wrap;
 `;
 
 export const HomePage = () => {
+  const isAuthenticated = useAppSelector(
+    (state) => state.authReducer.isAuthenticated,
+  );
+
   return (
     <Home>
       <Content>
@@ -135,45 +121,22 @@ export const HomePage = () => {
               по его ИНН
             </HeaderTitle>
             <HeaderDescription>
-              Комплексный анализ публикаций, получение данных в формате PDF на
-              электронную почту.
+              Комплексный анализ публикаций, получение данных <br /> в формате
+              PDF на электронную почту.
             </HeaderDescription>
-            <ButtonComponent
-              font={'medium'}
-              width={'fit-content'}
-              text={'Запросить данные'}
-            />
+            {isAuthenticated && (
+              <ButtonComponent
+                font={'medium'}
+                width={'fit-content'}
+                text={'Запросить данные'}
+              />
+            )}
           </HeaderLeft>
-          <img src={homeSvg1} alt={''} />
+          <HeaderSvg src={homeSvg1} alt={''} />
         </Header>
         <Section>
           <Subtitle>Почему именно мы</Subtitle>
-          <Carousel>
-            <LeftButton>
-              <StyledImage src={arrowLeft} alt={'Backwards'} />
-            </LeftButton>
-            <CarouselItem>
-              <StyledImage src={carouselSvg1} alt={''} />
-              <span>Высокая и оперативная скорость обработки заявки</span>
-            </CarouselItem>
-            <CarouselItem>
-              <StyledImage src={carouselSvg2} alt={''} />
-              <span>
-                Огромная комплексная база данных, обеспечивающая объективный
-                ответ на запрос
-              </span>
-            </CarouselItem>
-            <CarouselItem>
-              <StyledImage src={carouselSvg3} alt={''} />
-              <span>
-                Защита конфиденциальных сведений, не подлежащих разглашению по
-                федеральному законодательству
-              </span>
-            </CarouselItem>
-            <RightButton>
-              <StyledImage src={arrowRight} alt={'Forwards'} />
-            </RightButton>
-          </Carousel>
+          <CarouselComponent />
           <img src={homeSvg2} alt={''} />
         </Section>
         <Section>
