cb9cbaa9aabe447ef9b97f05251fc80616c3b69e
Dmitry <wenrait@gmail.com>
2024-06-10 23:19:08 +0300

    Placed Account Info in a separate component

--- src/components/Account.tsx
+++ src/components/Account.tsx
@@ -2,46 +2,13 @@ import styled from 'styled-components';
 import avatar from '../assets/svg/avatar.svg';
 import { setAuthData } from '../redux/authSlice.ts';
 import { useAppDispatch } from '../hooks.ts';
-import { useGetAccountInfoQuery } from '../api/accountInfoApi.ts';
+import { AccountInfoComponent } from './AccountInfo.tsx';
 
 const Account = styled.div`
+  width: 100%;
   display: flex;
-  gap: 130px;
-`;
-
-const Info = styled.div`
-  padding: 12px;
-  display: flex;
-  gap: 7px;
   align-items: center;
-  justify-content: center;
-  background: rgba(217, 217, 217, 0.3);
-  border-radius: 5px;
-  line-height: 12px;
-`;
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 8px;
-`;
-
-const Text = styled.span`
-  display: flex;
-  opacity: 40%;
-  font-size: 10px;
-  margin-left: auto;
-`;
-
-const Count = styled.span`
-  font-weight: 700;
-  font-size: 14px;
-`;
-
-const Limit = styled.span`
-  font-weight: 700;
-  font-size: 14px;
-  color: rgba(138, 197, 64, 1);
+  justify-content: space-between;
 `;
 
 const User = styled.div`
@@ -80,15 +47,6 @@ const Avatar = styled.img`
 
 export const AccountComponent = () => {
   const dispatch = useAppDispatch();
-
-  const { data, error } = useGetAccountInfoQuery({});
-  if (error) {
-    console.log(error);
-  }
-  if (data) {
-    console.log('User data: ', data.eventFiltersInfo);
-  }
-
   const handleClick = () => {
     try {
       dispatch(
@@ -105,19 +63,7 @@ export const AccountComponent = () => {
 
   return (
     <Account>
-      {data && (
-        <Info>
-          <Container>
-            <Text>Использовано компаний</Text>
-            <Text>Лимит по компаниям</Text>
-          </Container>
-          <Container>
-            <Count>{data.eventFiltersInfo.usedCompanyCount}</Count>
-            <Limit>{data.eventFiltersInfo.companyLimit}</Limit>
-          </Container>
-        </Info>
-      )}
-
+      <AccountInfoComponent />
       <User>
         <Content>
           <Username>Алексей А.</Username>
---
+++ src/components/AccountInfo.tsx
@@ -0,0 +1,67 @@
+import { useGetAccountInfoQuery } from '../api/accountInfoApi.ts';
+import styled from 'styled-components';
+
+const Info = styled.div`
+  padding: 12px;
+  display: flex;
+  gap: 7px;
+  align-items: center;
+  justify-content: center;
+  background: rgba(217, 217, 217, 0.3);
+  border-radius: 5px;
+  line-height: 12px;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
+const Text = styled.span`
+  display: flex;
+  opacity: 40%;
+  font-size: 10px;
+  margin-left: auto;
+  @media (max-width: 600px) {
+    font-size: 8px;
+  }
+`;
+
+const Count = styled.span`
+  font-weight: 700;
+  font-size: 14px;
+`;
+
+const Limit = styled.span`
+  font-weight: 700;
+  font-size: 14px;
+  color: rgba(138, 197, 64, 1);
+`;
+
+export const AccountInfoComponent = () => {
+  const { data, isLoading, error } = useGetAccountInfoQuery({});
+
+  if (isLoading) {
+    return <Info>Loading...</Info>;
+  }
+
+  if (error) {
+    return <Info>{error.data.message}</Info>;
+  }
+
+  if (data) {
+    return (
+      <Info>
+        <Container>
+          <Text>Использовано компаний</Text>
+          <Text>Лимит по компаниям</Text>
+        </Container>
+        <Container>
+          <Count>{data.eventFiltersInfo.usedCompanyCount}</Count>
+          <Limit>{data.eventFiltersInfo.companyLimit}</Limit>
+        </Container>
+      </Info>
+    );
+  }
+};
