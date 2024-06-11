ad75642c56373a9057569cced5a3a1cde73c8a23
Dmitry <wenrait@gmail.com>
2024-06-11 18:58:19 +0300

    Layout fixes

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -11,8 +11,12 @@ import { colors } from '../styles/globalStyles.ts';
 
 const Login = styled.div`
   display: flex;
+  align-items: center;
+  justify-content: space-between;
   padding: 0 60px;
+  max-width: 1440px;
   flex: 1;
+  box-sizing: border-box;
 `;
 
 const Left = styled.div`
@@ -44,6 +48,14 @@ const Form = styled.form`
   display: flex;
   flex-direction: column;
   gap: 30px;
+  position: relative;
+`;
+
+const Lock = styled.img`
+  position: absolute;
+  top: -50px;
+  left: -50px;
+  display: block;
 `;
 
 const AuthButtons = styled.div`
@@ -93,6 +105,62 @@ const Input = styled.input`
   padding: 12px 19px;
 `;
 
+const Auth = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  position: relative;
+`;
+
+const RecoveryLink = styled.a`
+  color: ${colors.secondary.blue};
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 16.94px;
+  letter-spacing: 0.02em;
+  text-align: center;
+  text-decoration: underline;
+`;
+
+const ExternalAuth = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+`;
+
+const ExternalAuthButtons = styled.div`
+  display: flex;
+  gap: 10px;
+`;
+
+const Text = styled.span`
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 19.36px;
+  letter-spacing: 0.02em;
+  color: rgba(148, 148, 148, 1);
+`;
+
+const ErrorMessage = styled.span`
+  width: 100%;
+  text-align: center;
+  color: rgba(255, 89, 89, 1);
+  font-size: 14px;
+  line-height: 16.94px;
+  letter-spacing: 0.01em;
+  position: absolute;
+  top: -25px;
+`;
+
+// const LoginViaButton = styled.button`
+//   background: transparent;
+//   display: flex;
+//   place-items: center;
+//   width: 96px;
+//   height: 31px;
+//   border-radius: 3px;
+//   border: 1px solid rgba(89, 112, 255, 0.51);
+// `;
 
 export const LoginPage = () => {
   const [login, setLogin] = useState('');
@@ -133,17 +201,20 @@ export const LoginPage = () => {
           на тариф, необходимо <br />
           авторизоваться.
         </Title>
-        <Key src={KeySvg} alt={'Key'} />
+        <Key src={keySvg} alt={'Key'} />
       </Left>
       <Right>
         <Form onSubmit={handleSubmit}>
-          <AuthButtons>
-            <AuthButton>Войти</AuthButton>
-            <AuthButton disabled>Зарегистрироваться</AuthButton>
-          </AuthButtons>
+          <Lock src={lockSvg} />
+          <AuthModeButtons>
+            <AuthModeButton>Войти</AuthModeButton>
+            <AuthModeButton disabled>Зарегистрироваться</AuthModeButton>
+          </AuthModeButtons>
           <Field>
             <Label htmlFor={'login'}>Логин или номер телефона:</Label>
             <Input
+              $isError={isError}
+              id={'input-login'}
               name={'login'}
               type={'text'}
               onChange={(e) => setLogin(e.target.value)}
@@ -152,19 +223,32 @@ export const LoginPage = () => {
           <Field>
             <Label htmlFor={'password'}>Пароль:</Label>
             <Input
+              $isError={isError}
+              id={'input-password'}
               name={'password'}
               type={'password'}
               onChange={(e) => setPassword(e.target.value)}
             />
           </Field>
-          <ButtonComponent
-            text={'Войти'}
-            font={'small'}
-            width={'100%'}
-            type={'submit'}
-            disabled={isLoading || !(login.length > 0 && password.length > 0)}
-          />
-          <div>{log}</div>
+          <Auth>
+            {isError && <ErrorMessage>{errorMessage}</ErrorMessage>}
+            <ButtonComponent
+              text={'Войти'}
+              font={'small'}
+              width={'100%'}
+              type={'submit'}
+              disabled={isLoading || !(login.length > 0 && password.length > 0)}
+            />
+            <RecoveryLink>Восстановить пароль</RecoveryLink>
+          </Auth>
+          <ExternalAuth>
+            <Text>Войти через:</Text>
+            <ExternalAuthButtons>
+              <img src={google} alt={'Google'} />
+              <img src={facebook} alt={'Facebook'} />
+              <img src={yandex} alt={'Яндекс'} />
+            </ExternalAuthButtons>
+          </ExternalAuth>
         </Form>
       </Right>
     </Login>
