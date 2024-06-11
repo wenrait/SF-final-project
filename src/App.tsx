005125f327c265711b74c08dfec12e189b6f39c0
Dmitry <wenrait@gmail.com>
2024-06-11 15:09:47 +0300

    Added wrapper to App and to each child component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,37 @@ import { Routes, Route } from 'react-router-dom';
 import { FooterComponent } from './layout/Footer.tsx';
 import { LoginPage } from './pages/Login.tsx';
 import { useAppSelector } from './hooks.ts';
+import styled from 'styled-components';
+import { colors } from './styles/globalStyles.ts';
+
+const AppWrapper = styled.body`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const HeaderWrapper = styled.header`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const MainWrapper = styled.main`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+`;
+
+const FooterWrapper = styled.footer`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: ${colors.primary.teal};
+`;
 
 function App() {
   const isAuthenticated = useAppSelector(
@@ -14,15 +45,23 @@ function App() {
   };
 
   return (
-    <>
-      <HeaderComponent />
-      <Routes>
-        <Route path={'/'} element={<HomePage />} />
-        {!isAuthenticated && <Route path={'/login'} element={<LoginPage />} />}
-        <Route path={'/*'} element={<ErrorMock />} />
-      </Routes>
-      <FooterComponent />
-    </>
+    <AppWrapper>
+      <HeaderWrapper>
+        <HeaderComponent />
+      </HeaderWrapper>
+      <MainWrapper>
+        <Routes>
+          <Route path={'/'} element={<HomePage />} />
+          {!isAuthenticated && (
+            <Route path={'/login'} element={<LoginPage />} />
+          )}
+          <Route path={'/*'} element={<ErrorMock />} />
+        </Routes>
+      </MainWrapper>
+      <FooterWrapper>
+        <FooterComponent />
+      </FooterWrapper>
+    </AppWrapper>
   );
 }
 
